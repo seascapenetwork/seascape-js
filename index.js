0a69aeb3498f8c10e2660834b2cb966fe38b5ea9
Medet Ahmetson <ahmetson@zoho.com>
2021-01-10 22:37:59 +0700

    add tests

---
+++ index.js
@@ -0,0 +1,39 @@
+//let seascape = require("./seascape.js");
+
+let printApy = async function(){
+    let lpMining = seascape.gameById(1);
+    await lpMining.loadContracts();
+    
+    // lp token address is a pair CWS-ETH on uniswap
+    //let lpToken = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
+    let lpToken = "0xdc935332d39a4c632864dbbed3cfdbf049fb9267";
+
+    let cwsAddress = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
+           
+    //let sessionId = await lpMining.setSessionId(lpToken);
+    let sessionId = 2;
+    
+    let session = await lpMining.setSession(sessionId);
+
+    // finally, can show APY
+    let prices = await seascape.cws.getPrice(lpMining.networkId, cwsAddress);
+
+    let apy = await lpMining.setApy(session, prices.cws);
+    console.log(`APY: ${apy} %`);
+};
+
+//printApy();
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('dist'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
+
