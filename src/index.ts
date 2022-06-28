9b3ed917caea51879edff13b60aaa24a37f7532e
ahmetson <ahmetson@zoho.com>
2022-06-28 16:52:24 +0300

    Include into Index the SeascapeCdnConfig, SeascapeAbiConfig and SeascapeAbi

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import { Provider } from "./provider";
 import Wallet from "./wallet";
 import ProofOfServer from "./proof-of-server";
 import Verifier from "./verifier";
-import * as CdnRead from "./cdn-config/read";
 import * as CdnWrite from "./cdn-config/write";
 import * as CdnUtil from "./cdn-config/util";
+import SeascapeCdnConfig from "./cdn-config/seascape-cdn-config";
+import SeascapeAbiConfig from "./cdn-config/seascape-abi-config";
+import SeascapeAbi from "./cdn-config/seascape-abi";
 import { ConfigPath, SmartcontractConfig, SmartcontractPath } from './cdn-config/util';
 
 export {
@@ -18,10 +20,12 @@ export {
     Verifier,
     Smartcontract,
     Provider,
-    CdnRead,
     CdnWrite,
     CdnUtil,
     ConfigPath,
     SmartcontractConfig,
-    SmartcontractPath
+    SmartcontractPath,
+    SeascapeCdnConfig,
+    SeascapeAbiConfig,
+    SeascapeAbi
 }
